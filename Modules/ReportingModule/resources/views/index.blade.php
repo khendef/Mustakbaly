21edee6ea7e1284349cc125bb67b8bf0c174a69e
BaraaJa <baraaja2000@gmail.com>
2026-01-26 21:59:49 +0300

    Remove ReportingModule from Course_Curriculum_Management branch

--- Modules/ReportingModule/resources/views/index.blade.php
+++
@@ -1,5 +0,0 @@
-<x-reportingmodule::layouts.master>
-    <h1>Hello World</h1>
-
-    <p>Module: {!! config('reportingmodule.name') !!}</p>
-</x-reportingmodule::layouts.master>
--- Modules/ReportingModule/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Modules\ReportingModule\Http\Controllers\ReportingModuleController;
-
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::resource('reportingmodules', ReportingModuleController::class)->names('reportingmodule');
-});
